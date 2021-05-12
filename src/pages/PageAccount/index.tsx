43743ad53dc9c4a3832ab7c74d0237efe23f438b
Boris Kov <bkovesh@gmail.com>
2021-05-12 18:32:31 +0300

    Add route balance/orders to account

--- src/pages/PageAccount/index.tsx
+++ src/pages/PageAccount/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link, Route, Switch, useLocation, useRouteMatch } from 'react-router-dom';
 import BigNumber from 'bignumber.js/bignumber';
 import cns from 'classnames';
 import moment from 'moment/moment';
@@ -32,22 +33,32 @@ const Zx = new Service0x();
 const CryptoCompare = new CryptoCompareService();
 
 export const PageAccount: React.FC = () => {
+  const match = useRouteMatch();
+  const location = useLocation();
+  const { pathname } = location;
+  // console.log('PageAccount match, location:', match, location);
+
   const [data, setData] = React.useState<TableType[]>([] as any);
   const [orders, setOrders] = React.useState<any[]>([]);
   const [pageCount, setPageCount] = React.useState<number>(1);
+  const [flagSort, setFlagSort] = React.useState<string>('');
+
   const { tokens } = useSelector(({ zx }: any) => zx);
+  const { address: userAddress = 'Address', balance: userBalance = 0 } = useSelector(
+    ({ user }: any) => user,
+  );
 
   const [isPending, setIsPending] = React.useState<boolean>(true);
   const [sortFlagChanged, setSortFlagChanged] = React.useState<boolean>(false);
   const [activeColumn, setActiveColumn] = React.useState<string>('');
 
+  const isWide = useMedia({ minWidth: '767px' });
+  const isBalancePage = pathname === '/account';
+  const isOrdersPage = pathname === '/account/orders';
+
   const countDataForPagination = React.useCallback((): void => {
     setPageCount(Math.ceil(data.length / 12));
   }, [data.length]);
-
-  React.useEffect(() => {
-    countDataForPagination();
-  }, [countDataForPagination]);
 
   const getExchangeOfPair = React.useCallback(
     async ({ symbolOne, symbolTwo }: any): Promise<any> => {
@@ -109,7 +120,7 @@ export const PageAccount: React.FC = () => {
 
   const fillData = React.useCallback(async (): Promise<void> => {
     setIsPending(true);
-    console.log('Loading *account table data*...');
+    console.log('PageAccount fillData:', 'loading...');
     const dataForTableLocal: any = [];
     const arr = [...orders];
 
@@ -155,22 +166,10 @@ export const PageAccount: React.FC = () => {
       });
     }
     setIsPending(false);
-    console.log('DONE! *account table data*');
+    console.log('PageAccount fillData:', 'loaded');
     setData([...dataForTableLocal]);
     setDataForTable([...dataForTableLocal].slice(0, 12));
   }, [findToken, getExchangeOfPair, getHistory, orders]);
-
-  React.useEffect(() => {
-    getOrders(address);
-  }, [getOrders]);
-
-  React.useEffect(() => {
-    fillData();
-  }, [fillData]);
-
-  const isWide = useMedia({ minWidth: '767px' });
-
-  const [flagSort, setFlagSort] = React.useState<string>('');
 
   const onSort = (param: string): void => {
     if (isPending) {
@@ -187,192 +186,216 @@ export const PageAccount: React.FC = () => {
     setDataForTable(arg);
     setSortFlagChanged(false);
   };
-  React.useEffect(() => {}, [data, dataForTable]);
 
-  const { address: userAddress = 'Address', balance: userBalance = 0 } = useSelector(
-    ({ user }: any) => user,
-  );
+  React.useEffect(() => {
+    getOrders(address);
+  }, [getOrders]);
+
+  React.useEffect(() => {
+    fillData();
+  }, [fillData]);
+
+  React.useEffect(() => {
+    countDataForPagination();
+  }, [countDataForPagination]);
 
   return (
     <div className={s.container}>
       <section className={s.containerTitle}>
         <div className={s.containerTitleBlock}>
           <h1>Your Account</h1>
-          <span>{userAddress === '' ? '0xca807vad7fv...' : userAddress}</span>
+          <span>{userAddress === '' ? '' : userAddress}</span>
         </div>
         <div className={s.accountMenu}>
-          <span className={s.accountMenuActive}>Balance</span>
-          <span className={s.accountMenuItem}>Trade History</span>
+          <Link
+            to={`${match.url}`}
+            className={isBalancePage ? s.accountMenuItemctive : s.accountMenuItem}
+          >
+            Balance
+          </Link>
+          <Link
+            to={`${match.url}/orders`}
+            className={isOrdersPage ? s.accountMenuItemActive : s.accountMenuItem}
+          >
+            Orders History
+          </Link>
         </div>
       </section>
 
-      <section className={s.accountFunds}>
-        <div className={s.accountFundsItem}>
-          <div className={s.accountFundsCard}>
-            <h3>Your balance:</h3>
-            <span>{userBalance} ETH</span>
-            <img src={EthGlassIcon} alt="ehereum logo" />
-          </div>
-          <div className={s.accountFundsDeposit}>
-            <h3>Deposit more funds</h3>
-            <div className={s.accountFundsDepositCopy}>
-              <span>0x9C3fD81263...337317c0404d</span>
-              <img src={CopyIcon} alt="copy icon" />
+      <Switch>
+        <Route path={match.path} exact>
+          <section className={s.accountFunds}>
+            <div className={s.accountFundsItem}>
+              <div className={s.accountFundsCard}>
+                <h3>Your balance:</h3>
+                <span>{userBalance} ETH</span>
+                <img src={EthGlassIcon} alt="ehereum logo" />
+              </div>
+              <div className={s.accountFundsDeposit}>
+                <h3>Deposit more funds</h3>
+                <div className={s.accountFundsDepositCopy}>
+                  <span>0x9C3fD81263...337317c0404d</span>
+                  <img src={CopyIcon} alt="copy icon" />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </section>
+          </section>
+        </Route>
+        <Route path={`${match.path}/orders`}>
+          <section className={s.accountTrade}>
+            <h2>Order History</h2>
 
-      <section className={s.accountTrade}>
-        <h2>Order History</h2>
-
-        <div className={s.accountTradeHistory}>
-          <table className={s.accountTradeTable}>
-            {isWide ? (
-              <thead>
-                <tr>
-                  <th
-                    className={cns(activeColumn === 'timestart' ? s.accountTradeTableActive : null)}
-                    onClick={onSort.bind(this, 'timestart')}
-                  >
-                    Time start
-                  </th>
-                  <th>Time end</th>
-                  <th>Trading pair</th>
-                  <th>Amount</th>
-                  <th>Price</th>
-                </tr>
-              </thead>
-            ) : (
-              <thead>
-                <tr>
-                  <th className={s.accountTradeTableActive} onClick={onSort.bind(this, 'name')}>
-                    Token
-                  </th>
-                  <th> </th>
-                  <th onClick={onSort.bind(this, 'priceChange')}>Last 24h</th>
-                </tr>
-              </thead>
-            )}
-            {
-              isWide ? (
-                <tbody>
-                  {dataForTable.map((item: any) => {
-                    const { orderCreate, orderExpire, price, tradingPair, amount } = item;
-                    let priceChangeModel = (
-                      <td>
-                        <img src={ArrowUpIcon} alt="arrow up" /> {`$${price}`}
-                      </td>
-                    );
-                    if (price < 0) {
-                      priceChangeModel = (
-                        <td className={`${s.accountTradeTableDown}`}>
-                          <img src={ArrowDownIcon} alt="arrow down" /> {`$${price}`}
-                        </td>
-                      );
-                    }
-                    if (price === 0) {
-                      priceChangeModel = <td>{`$${price}`}</td>;
-                    }
-                    return (
-                      <tr key={uuid()}>
-                        <td>{orderCreate}</td>
-                        <td>{orderExpire}</td>
-                        <td>
-                          {tradingPair.symbolMaker} / {tradingPair.symbolTaker}
-                        </td>
-                        <td>{amount}</td>
-                        {priceChangeModel}
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              ) : null
-              // mobile table body waits for a new design...
-              // <tbody>
-              //   {tableData.map((token: TableType) => {
-              //     const { name, symbol, price, priceChange } = token;
-              //
-              //     let priceChangeModel = (
-              //       <td>
-              //         <img src={ArrowUpIcon} alt="arrow up" /> {`${priceChange}`}%
-              //       </td>
-              //     );
-              //
-              //     if (priceChange < 0) {
-              //       priceChangeModel = (
-              //         <td className={`${s.accountTradeTableDown}`}>
-              //           <img src={ArrowDownIcon} alt="arrow down" />
-              //           {priceChange}%
-              //         </td>
-              //       );
-              //     }
-              //
-              //     return (
-              //       <tr>
-              //         <td>{name}</td>
-              //         <td>{symbol}</td>
-              //         <td>{price}$</td>
-              //         {priceChangeModel}
-              //       </tr>
-              //     );
-              //   })}
-              // </tbody>
-              // <tbody>
-              //   {tableData.map((token: TableType, index: number) => {
-              //     const { symbol, price, priceChange } = token;
-              //
-              //     let priceChangeModel = (
-              //       <td className={`${s.mobilePriceChangeModel}`}>
-              //         <div className={s.flexContainerRow}>
-              //           <img src={ArrowUpIcon} alt="arrow up" />
-              //           {`${priceChange}`}%
-              //         </div>
-              //       </td>
-              //     );
-              //
-              //     if (priceChange < 0) {
-              //       priceChangeModel = (
-              //         <td className={`${s.accountTradeTableDown} ${s.mobilePriceChangeModel}`}>
-              //           <img src={ArrowDownIcon} alt="arrow down" />
-              //           {priceChange}%
-              //         </td>
-              //       );
-              //     }
-              //
-              //     return (
-              //       <>
-              //         {index < 5 && (
-              //           <tr>
-              //             <td>
-              //               {dataForTable.orderCreate}
-              //               <div className={s.mobileSymbol}>{symbol}</div>
-              //             </td>
-              //             <td />
-              //             <td>
-              //               <div className={s.mobilePriceAndChangeContainer}>
-              //                 <div className={s.mobilePrice}>${price}</div>
-              //                 {priceChangeModel}
-              //               </div>
-              //             </td>
-              //           </tr>
-              //         )}
-              //       </>
-              //     );
-              //   })}
-              // </tbody>
-            }
-          </table>
-        </div>
-      </section>
-      <section className={s.paginationContainer}>
-        <Pagination
-          pageCount={pageCount}
-          emitChanges={emitChanges}
-          data={data}
-          sortFlagChanged={sortFlagChanged}
-        />
-      </section>
+            <div className={s.accountTradeHistory}>
+              <table className={s.accountTradeTable}>
+                {isWide ? (
+                  <thead>
+                    <tr>
+                      <th
+                        className={cns(
+                          activeColumn === 'timestart' ? s.accountTradeTableActive : null,
+                        )}
+                        onClick={onSort.bind(this, 'timestart')}
+                      >
+                        Time start
+                      </th>
+                      <th>Time end</th>
+                      <th>Trading pair</th>
+                      <th>Amount</th>
+                      <th>Price</th>
+                    </tr>
+                  </thead>
+                ) : (
+                  <thead>
+                    <tr>
+                      <th className={s.accountTradeTableActive} onClick={onSort.bind(this, 'name')}>
+                        Token
+                      </th>
+                      <th> </th>
+                      <th onClick={onSort.bind(this, 'priceChange')}>Last 24h</th>
+                    </tr>
+                  </thead>
+                )}
+                {
+                  isWide ? (
+                    <tbody>
+                      {dataForTable.map((item: any) => {
+                        const { orderCreate, orderExpire, price, tradingPair, amount } = item;
+                        let priceChangeModel = (
+                          <td>
+                            <img src={ArrowUpIcon} alt="arrow up" /> {`$${price}`}
+                          </td>
+                        );
+                        if (price < 0) {
+                          priceChangeModel = (
+                            <td className={`${s.accountTradeTableDown}`}>
+                              <img src={ArrowDownIcon} alt="arrow down" /> {`$${price}`}
+                            </td>
+                          );
+                        }
+                        if (price === 0) {
+                          priceChangeModel = <td>{`$${price}`}</td>;
+                        }
+                        return (
+                          <tr key={uuid()}>
+                            <td>{orderCreate}</td>
+                            <td>{orderExpire}</td>
+                            <td>
+                              {tradingPair.symbolMaker} / {tradingPair.symbolTaker}
+                            </td>
+                            <td>{amount}</td>
+                            {priceChangeModel}
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  ) : null
+                  // mobile table body waits for a new design...
+                  // <tbody>
+                  //   {tableData.map((token: TableType) => {
+                  //     const { name, symbol, price, priceChange } = token;
+                  //
+                  //     let priceChangeModel = (
+                  //       <td>
+                  //         <img src={ArrowUpIcon} alt="arrow up" /> {`${priceChange}`}%
+                  //       </td>
+                  //     );
+                  //
+                  //     if (priceChange < 0) {
+                  //       priceChangeModel = (
+                  //         <td className={`${s.accountTradeTableDown}`}>
+                  //           <img src={ArrowDownIcon} alt="arrow down" />
+                  //           {priceChange}%
+                  //         </td>
+                  //       );
+                  //     }
+                  //
+                  //     return (
+                  //       <tr>
+                  //         <td>{name}</td>
+                  //         <td>{symbol}</td>
+                  //         <td>{price}$</td>
+                  //         {priceChangeModel}
+                  //       </tr>
+                  //     );
+                  //   })}
+                  // </tbody>
+                  // <tbody>
+                  //   {tableData.map((token: TableType, index: number) => {
+                  //     const { symbol, price, priceChange } = token;
+                  //
+                  //     let priceChangeModel = (
+                  //       <td className={`${s.mobilePriceChangeModel}`}>
+                  //         <div className={s.flexContainerRow}>
+                  //           <img src={ArrowUpIcon} alt="arrow up" />
+                  //           {`${priceChange}`}%
+                  //         </div>
+                  //       </td>
+                  //     );
+                  //
+                  //     if (priceChange < 0) {
+                  //       priceChangeModel = (
+                  //         <td className={`${s.accountTradeTableDown} ${s.mobilePriceChangeModel}`}>
+                  //           <img src={ArrowDownIcon} alt="arrow down" />
+                  //           {priceChange}%
+                  //         </td>
+                  //       );
+                  //     }
+                  //
+                  //     return (
+                  //       <>
+                  //         {index < 5 && (
+                  //           <tr>
+                  //             <td>
+                  //               {dataForTable.orderCreate}
+                  //               <div className={s.mobileSymbol}>{symbol}</div>
+                  //             </td>
+                  //             <td />
+                  //             <td>
+                  //               <div className={s.mobilePriceAndChangeContainer}>
+                  //                 <div className={s.mobilePrice}>${price}</div>
+                  //                 {priceChangeModel}
+                  //               </div>
+                  //             </td>
+                  //           </tr>
+                  //         )}
+                  //       </>
+                  //     );
+                  //   })}
+                  // </tbody>
+                }
+              </table>
+            </div>
+          </section>
+          <section className={s.paginationContainer}>
+            <Pagination
+              pageCount={pageCount}
+              emitChanges={emitChanges}
+              data={data}
+              sortFlagChanged={sortFlagChanged}
+            />
+          </section>
+        </Route>
+      </Switch>
     </div>
   );
 };
