c4539687f62bd3e4007d13ff7bbf400c4ff5675e
VasilievIgor1 <vasiliev.igor@mywish.io>
2021-05-20 15:01:13 +0300

    Fix waiting button

--- src/pages/PageMarkets/PageMarketsContent/index.tsx
+++ src/pages/PageMarkets/PageMarketsContent/index.tsx
@@ -1378,7 +1378,7 @@ export const PageMarketsContent: React.FC = () => {
           </div>
         </div>
         <div className={s.containerTradingButton}>
-          <Button onClick={handleTrade} disabled={isTradeDisabled}>
+          <Button onClick={handleTrade} disabled={isTradeDisabled || waiting}>
             {!userAddress ? 'Connect wallet' : waiting ? 'Waiting...' : 'Trade'}
           </Button>
         </div>
