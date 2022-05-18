f16e44b78ca1b02d0a9f3485bf71f43d280887a1
Alexey Shevchenko <kamikozz@mail.ru>
2022-05-19 00:37:05 +0300

    feat: add left column for 3 staking options on Stake page

--- src/pages/PageStake/index.tsx
+++ src/pages/PageStake/index.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
 import { noop } from 'lodash';
 
+import ethToken from '../../data/ethToken';
 import gearToken from '../../data/gearToken';
 
-import { Banner, Reward, Stake, TooltipApr, TooltipApy } from './components';
+import { Banner, Reward, SectionHead, Stake, TooltipApr, TooltipApy } from './components';
 
 import styles from './PageStake.module.scss';
 
@@ -12,48 +13,146 @@ export const PageStake: FC = () => {
     <div className={styles.container}>
       <Banner apy={160.41} />
       <div className={styles.section}>
-        APR
-        <TooltipApr
-          tokenSymbol1={gearToken.symbol}
-          tokenSymbol2="ETH"
-          tokenApr1="21,92"
-          tokenApr2="46,01"
+        <SectionHead
+          title="LP Token Staking"
+          stakeToken={gearToken.symbol}
+          earnToken={ethToken.symbol}
+          totalStaked={{
+            token: '164,869,690',
+            usd: '265,876,000',
+          }}
+          // eslint-disable-next-line prettier/prettier
+          performance={(
+            <>
+              166,01% APR
+              <div className={styles.infoIcon}>
+                <TooltipApr
+                  tokenSymbol1={gearToken.symbol}
+                  tokenSymbol2="ETH"
+                  tokenApr1="21,92"
+                  tokenApr2="46,01"
+                />
+              </div>
+            </>
+            // eslint-disable-next-line prettier/prettier
+          )}
         />
-        Stake container
-        <Stake
-          onStakeClick={noop}
-          onUnstakeClick={noop}
-          tokenName={gearToken.symbol}
-          stakeAmount={0}
-          tokenBalance={25}
-        />
+        <div className={styles.sectionBody}>
+          <Stake
+            onStakeClick={noop}
+            onUnstakeClick={noop}
+            tokenName={gearToken.symbol}
+            stakeAmount={0}
+            tokenBalance={25}
+          />
+          <Reward
+            tokenName={gearToken.symbol}
+            stakeAmount={0}
+            ethReward={75}
+            lastCollectedTimestamp={1652703791}
+            collectedToDate={40}
+            earnedToDate={30}
+            onCollectRewardClick={noop}
+          />
+        </div>
       </div>
 
-      <div>
-        GEAR Compounder APY
-        <TooltipApy
-          token1={{
-            symbol: gearToken.symbol,
-            apr: '21,60',
+      <div className={styles.section}>
+        <SectionHead
+          title={`${gearToken.symbol} Staking`}
+          stakeToken={gearToken.symbol}
+          earnToken={ethToken.symbol}
+          totalStaked={{
+            token: '164,869,690',
+            usd: '265,876,000',
           }}
-          token2={{
-            symbol: 'WETH',
-            apr: '40,79',
-            apy: '50,37',
-          }}
-          dailyEstimatedCompounds={446}
+          // eslint-disable-next-line prettier/prettier
+          performance={(
+            <>
+              166,01% APR
+              <div className={styles.infoIcon}>
+                <TooltipApr
+                  tokenSymbol1={gearToken.symbol}
+                  tokenSymbol2="ETH"
+                  tokenApr1="21,92"
+                  tokenApr2="46,01"
+                />
+              </div>
+            </>
+            // eslint-disable-next-line prettier/prettier
+          )}
         />
+        <div className={styles.sectionBody}>
+          <Stake
+            onStakeClick={noop}
+            onUnstakeClick={noop}
+            tokenName={gearToken.symbol}
+            stakeAmount={0}
+            tokenBalance={25}
+          />
+          <Reward
+            tokenName={gearToken.symbol}
+            stakeAmount={0}
+            ethReward={75}
+            lastCollectedTimestamp={1652703791}
+            collectedToDate={40}
+            earnedToDate={30}
+            onCollectRewardClick={noop}
+          />
+        </div>
       </div>
 
-      <Reward
-        tokenName={gearToken.symbol}
-        stakeAmount={0}
-        ethReward={75}
-        lastCollectedTimestamp={1652703791}
-        collectedToDate={40}
-        earnedToDate={30}
-        onCollectRewardClick={noop}
-      />
+      <div className={styles.section}>
+        <SectionHead
+          title={`${gearToken.symbol} Compounder`}
+          isCompounder
+          stakeToken={gearToken.symbol}
+          earnToken={gearToken.symbol}
+          totalStaked={{
+            token: '164,869,690',
+            usd: '265,876,000',
+          }}
+          // eslint-disable-next-line prettier/prettier
+          performance={(
+            <>
+              166,01% APY
+              <div className={styles.infoIcon}>
+                <TooltipApy
+                  token1={{
+                    symbol: gearToken.symbol,
+                    apr: '21,60',
+                  }}
+                  token2={{
+                    symbol: 'WETH',
+                    apr: '40,79',
+                    apy: '50,37',
+                  }}
+                  dailyEstimatedCompounds={446}
+                />
+              </div>
+            </>
+            // eslint-disable-next-line prettier/prettier
+          )}
+        />
+        <div className={styles.sectionBody}>
+          <Stake
+            onStakeClick={noop}
+            onUnstakeClick={noop}
+            tokenName={gearToken.symbol}
+            stakeAmount={0}
+            tokenBalance={25}
+          />
+          <Reward
+            tokenName={gearToken.symbol}
+            stakeAmount={0}
+            ethReward={75}
+            lastCollectedTimestamp={1652703791}
+            collectedToDate={40}
+            earnedToDate={30}
+            onCollectRewardClick={noop}
+          />
+        </div>
+      </div>
     </div>
   );
 };
