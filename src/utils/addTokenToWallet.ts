8e5782c611f478c0190691f9c0d55e48d0307a00
Alexey Shevchenko <kamikozz@mail.ru>
2022-05-19 17:52:02 +0300

    feat: add token to wallet button (Stake/Banner)

---
+++ src/utils/addTokenToWallet.ts
@@ -0,0 +1,40 @@
+type Token = {
+  address: string;
+  symbol: string;
+  decimals: number;
+  image?: string;
+};
+
+/**
+ *  @see https://docs.metamask.io/guide/registering-your-token.html#example
+ */
+export const addTokenToWallet = async ({
+  address,
+  decimals,
+  symbol,
+  image = '',
+}: Token): Promise<boolean> => {
+  try {
+    // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+    const wasAdded = await window.ethereum.request({
+      method: 'wallet_watchAsset',
+      params: {
+        type: 'ERC20', // Initially only supports ERC20, but eventually more!
+        options: {
+          address, // The address that the token is at.
+          symbol, // A ticker symbol or shorthand, up to 5 chars.
+          decimals, // The number of decimals in the token
+          image, // A string url of the token logo
+        },
+      },
+    });
+
+    if (wasAdded) {
+      return true;
+    }
+    return false;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
