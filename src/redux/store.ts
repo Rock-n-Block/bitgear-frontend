ab86674e723fbcb96a08e3f82af86626fcb9ba48
Alexey Shevchenko <kamikozz@mail.ru>
2022-05-22 19:03:00 +0300

    refactor: remove ambiguous ts-ignore & eslint rules for redux/store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,14 @@ import { createStore } from 'redux';
 
 import rootReducer from './reducers/index';
 
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__: any;
+  }
+}
+
 const store = createStore(
   rootReducer,
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  // eslint-disable-next-line no-param-reassign
   // eslint-disable-next-line no-underscore-dangle
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
