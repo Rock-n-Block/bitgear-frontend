65f4d387f4a8ff367f1f1ef94c80c0a0bdef7818
Alexey Shevchenko <kamikozz@mail.ru>
2022-05-24 12:36:12 +0300

    fix: useWeb3Provider is calling web3Provider that is retrieved from useWalletConnectorContext() as a function, but it's object

--- src/hooks/useWeb3Provider.ts
+++ src/hooks/useWeb3Provider.ts
@@ -9,7 +9,7 @@ type UseWeb3ProviderReturnType = { web3Provider: Web3 };
 export const useWeb3Provider = (): UseWeb3ProviderReturnType => {
   const { web3Provider: web3ProviderFromContext } = useWalletConnectorContext();
   const web3Provider = useMemo(() => {
-    if (web3ProviderFromContext) return web3ProviderFromContext.web3Provider();
+    if (web3ProviderFromContext) return web3ProviderFromContext.web3Provider;
     return getWeb3();
   }, [web3ProviderFromContext]);
   return { web3Provider };
