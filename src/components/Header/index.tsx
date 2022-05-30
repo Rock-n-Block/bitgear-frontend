a4cf678dfd3f1ec0a1fcffd02ba68db01b867598
Alexey Shevchenko <kamikozz@mail.ru>
2022-05-31 02:34:59 +0300

    fix: replace Stake section from Header with Farm

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -427,24 +427,13 @@ export const Header: React.FC = () => {
             onClick={handleCloseDropdownMenuMobile}
           >
             <NavLink
-              to="/stake"
-              isActive={() => window.location.pathname === '/stake'}
+              to="/farm"
+              isActive={() => window.location.pathname === '/farm'}
               activeClassName={s.headerItemActive}
               onClick={() => setOpenMenu(false)}
             >
-              Stake
-            </NavLink>
-          </div>
-          <div
-            className={s.headerItem}
-            role="button"
-            tabIndex={0}
-            onKeyDown={() => {}}
-            onClick={handleCloseDropdownMenuMobile}
-          >
-            <a href="https://farm.bitgear.io" target="_blank" rel="noreferrer">
               Farm
-            </a>
+            </NavLink>
           </div>
           <div className={s.headerItemDesktop}>
             <NetworkDropDown />
