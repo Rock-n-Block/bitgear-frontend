5dc9394cdd83918867aabdf42f096e9caf6a5dcc
Alexey Shevchenko <kamikozz@mail.ru>
2022-06-07 15:57:35 +0300

    chore: change stake LP token from KingOfLp to Uniswapv2pair

--- src/data/gearEthLPToken.ts
+++ src/data/gearEthLPToken.ts
@@ -9,7 +9,7 @@ export default {
     if (config.IS_PRODUCTION) return '0xdd5d1a256b25e1087fc3b098b443e96cfa73237d';
     if (config.TESTING_NET === 'ropsten') return '';
     if (config.TESTING_NET === 'kovan') return '';
-    return '0xa43fdccf4d23d5ec7b6f2f946e2dd17a9b6c1e8d';
+    return '0xd460FCF4730C9f910DAE1FB9AE3dF93D8D3FD4AD';
   })(),
   image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/6593.png',
 };
