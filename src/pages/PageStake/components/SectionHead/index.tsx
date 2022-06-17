c369a08af787f2253fcb825ec22001d12c0a45e0
Alexey Shevchenko <kamikozz@mail.ru>
2022-06-17 14:32:42 +0300

    feat(staking): replace SectionHead stake text for LP & regular GEAR

--- src/pages/PageStake/components/SectionHead/index.tsx
+++ src/pages/PageStake/components/SectionHead/index.tsx
@@ -1,6 +1,8 @@
 import { FC, ReactNode } from 'react';
+import cn from 'classnames';
 
 import { bitGearTokenIcon, compounderBlueVariantIcon } from '../../../../assets/icons';
+import { ReactComponent as EthIconSvg } from '../../../../assets/icons/tokens/eth.svg';
 import { SkeletonLoader } from '../../../../components';
 import { getFormattedValue, numberTransform } from '../../../../utils';
 import { TooltipValue } from '../TooltipValue';
@@ -32,11 +34,24 @@ export const SectionHead: FC<SectionHeadProps> = ({
   const isStakeLpToken = stakeToken.includes('-');
   return (
     <div className={styles.sectionHead}>
-      <div className={styles.sectionHeadLogo}>
+      <div
+        className={cn(styles.sectionHeadLogo, {
+          [styles.sectionHeadLogoLp]: isStakeLpToken,
+        })}
+      >
         {isCompounder ? (
           <>
             <img src={compounderBlueVariantIcon} alt="compounder icon" />
-            <img className={styles.pairedToken} src={bitGearTokenIcon} alt="gear token icon" />
+            <img
+              className={cn(styles.pairedToken, styles.pairedTokenStake)}
+              src={bitGearTokenIcon}
+              alt="gear token icon"
+            />
+          </>
+        ) : isStakeLpToken ? (
+          <>
+            <img src={bitGearTokenIcon} alt="gear token icon" />
+            <EthIconSvg className={cn(styles.pairedToken)} />
           </>
         ) : (
           <img src={bitGearTokenIcon} alt="gear token icon" />
@@ -44,17 +59,26 @@ export const SectionHead: FC<SectionHeadProps> = ({
       </div>
       <div className={styles.sectionHeadInfo}>
         <div className={styles.sectionHeadInfoTitle}>{title}</div>
-        <div className={styles.sectionHeadInfoSubtitle}>
-          Stake{' '}
-          <span>
-            {isStakeLpToken && 'Uniswap v2'} {stakeToken}
-          </span>{' '}
-          <div>|</div> Earn {earnToken}
-        </div>
-        <div className={styles.sectionHeadInfoDescription}>
-          {earnToken} rewards auto compound into more{' '}
-          {isStakeLpToken ? stakeToken.split('-')[0] : stakeToken}!
-        </div>
+        {isStakeLpToken ? (
+          <div className={styles.sectionHeadInfoSubtitle}>
+            Stake <span>LP tokens</span> to double your reward
+          </div>
+        ) : isCompounder ? (
+          <>
+            <div className={styles.sectionHeadInfoSubtitle}>
+              Stake <span>{stakeToken}</span> <div>|</div> Earn {earnToken}
+            </div>
+            <div className={styles.sectionHeadInfoDescription}>
+              {earnToken} rewards auto compound into more {stakeToken}!
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.sectionHeadInfoSubtitle}>
+              Stake <span>{stakeToken}</span> to earn more and more {earnToken}
+            </div>
+          </>
+        )}
         <TooltipValue
           // eslint-disable-next-line prettier/prettier
           target={(
